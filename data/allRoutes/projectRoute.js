5c2e580480b0dd256895f4041bd0d45f1a9839fc
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-11 15:08:58 -0500

    "som conditionals added, and get project actions included. "

--- data/allRoutes/projectRoute.js
+++ data/allRoutes/projectRoute.js
@@ -30,17 +30,33 @@ router.get('/:id', (req, res) => {
             .json({ error: 'Failure, no projects! Try again.'})});
 });
 
+router.get('/:projectsId/actions', (req, res) => {
+    const projectsId = req.params.id;
+    prDB.getProjectActions(projectsId).then(thisPrsAss => {
+        if (!thisPrsAss.length) {
+            return res.status(404).json({error: 'No project/Actions Headers,Try again.', err});
+        }
+        res.status(200).json(thisPrsAss);
+    }).catch(err => {
+        res.status(500).json({error: 'Failure, no projects! Try again.', err})
+    })
+});
+
 //++++++++++++++++++++++++++++++++++++++++
 // - post stuff here
 //++++++++++++++++++++++++++++++++++++++++
 router.post('/', (req, res) => {
-    const { name, description, completed } = req.body;
-    if (projects.description && project.name) {
-        prDB.insert(project).then( nextProject => {
+    const newPr = req.body;
+    if (newPr.name.length > 128) {
+        return res.status(400)
+          .json({ error: "Shorten name bellow 128 characters", err });
+      }
+    if (newPr.description && newPr.name) {
+        prDB.insert(newPr).then( nextProject => {
             res.status(201).json({nextProject});
             console.log(nextProject);
-            console.log(project);
-        }).catch(err => { res.status(500).json({error: 'No post imput Added, Try again.', err})})
+            console.log(newPr);
+        }).catch(err => { res.status(500).json({error: 'No post imput Added, Try again.', err })})
     } else {
         res.status(404).json({error: 'You are missing either a name or a discription', err})
     }
@@ -52,7 +68,11 @@ router.post('/', (req, res) => {
 
 router.delete('/:id', (req,res) => {
     const id = req.params.id;
-    prDB.remove(id).then(count => {
+    prDB.remove(id).then(delEach => {
+        if (!delEach) {
+            res.status(404)
+                .json({ error: "No project deleted!", err });
+          }
         res.status(200).json({message: 'Item Deleted'});
     }).catch( err => {
         res.status(500).json({ error:'Nothing Was Deleted, Please Try again', err })
@@ -66,9 +86,16 @@ router.delete('/:id', (req,res) => {
 router.put('/:id', (req, res) => {
     const id = req.params.id;
     const project = req.body;
+    if (project.name.length > 128 || !project.name || !project.description) {
+        return res.status(400)
+            .json({ error: "Error name to log or missing name and/or description.", err });
+      }
     if (project.description && project.name) {
-        prDB.update(id,project).then( refreshedPR => {
-            res.status(204).json({message:`Updated ${refreshedPR}`});
+        prDB.update(id, project).then( refreshedPR => {
+            if (refreshedPR === null || refreshedPR === undefined) {
+                res.status(404).join({ error: 'No project here, Try again', err });
+            }
+            res.status(200).json({message:`Updated ${refreshedPR}`});
         }).catch(err => {
             res.status(500).json({ error: 'No Project Updated!', err})
         })
