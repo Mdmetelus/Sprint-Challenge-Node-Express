f8bd5cc589f2089a2a4ce0f7ebc521a17c2d5be7
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-11 12:16:23 -0500

    "update, middleware"

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const morganLogger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+
+
 const server = express();
+
+
+const parser = express.json();
+
+
+server.use( parser, morganLogger('short'), helmet(), cors(),)
+
+
+
 
 
 const port = process.env.PORT || 5050;
